561e21eb3da7baea6c8ebdf1e32983ab0c38e8b4
周诚 <chengzcom0926@gmail.com>
2023-08-16 12:28:13 +0800

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,8 +1,20 @@
-import resource
+import ctypes
 
-# 设置核心文件的大小限制（以字节为单位）
-resource.setrlimit(resource.RLIMIT_CORE, (1, resource.RLIM_INFINITY))
+# 定义一个C语言结构体
+class MyStruct(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_int)]
 
-# 生成一个除以零错误，导致崩溃并生成核心文件
-x = 1 / 0
+# 创建一个MyStruct实例
+my_struct = MyStruct()
 
+# 获取指向MyStruct实例的指针
+pointer = ctypes.pointer(my_struct)
+
+# 将指针强制转换为整数类型
+address = ctypes.cast(pointer, ctypes.c_void_p).value
+
+# 把一个无效的整数地址赋给指针
+invalid_address = address + 1
+ctypes.cast(invalid_address, ctypes.POINTER(MyStruct))
+
+# 这里会引发非法内存访问错误
